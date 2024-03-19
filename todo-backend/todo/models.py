657d7f047924ee3b95764d1ffc7cfb734dcbe26d
mn-48 <nazmul.cse48@gmail.com>
2024-03-19 15:59:36 +0600

    todos api

--- todo-backend/todo/models.py
+++ todo-backend/todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+
+class Todo(models.Model):
+    user = models.ForeignKey(User,  on_delete=models.CASCADE, related_name="todos")
+    title = models.CharField(max_length=255)
+    timestamp = models.DateField(auto_now_add=True)
+    place = models.CharField(max_length=255)
+    date_time = models.DateTimeField()
+    
+    def __str__(self):
+        return self.title 
+    
+    
+    
---
+++ todo-backend/todo/schema.py
@@ -0,0 +1,21 @@
+import graphene
+
+from graphene_django import DjangoObjectType
+
+from .models import Todo
+
+
+class TodoType(DjangoObjectType):
+    class Meta:
+        fields = "__all__"
+        model = Todo
+        
+class Query(graphene.ObjectType):
+    todo = graphene.Field(TodoType, id=graphene.Int())
+    todos = graphene.List(TodoType)
+    
+    def resolve_todo(root, info, id):
+        user = info.context.user
+        return Todo.objects.get(id=id, user=user)
+ 
+ schema = graphene.Schema(query=Query)
