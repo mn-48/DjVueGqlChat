8a0f51662198c13875af580f58a7f1bd5652d5d2
mn-48 <nazmul.cse48@gmail.com>
2024-03-19 15:37:01 +0600

    users app

--- todo-backend/config/settings.py
+++ todo-backend/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     
+    "Users",
     "todo",
     "graphene_django",
 ]
@@ -120,3 +121,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+AUTH_USER_MODEL = 'users.User'
